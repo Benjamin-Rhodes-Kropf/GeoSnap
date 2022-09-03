f58dec603d956871ca1cdb0fad0ee362f8cb4701
Ben Rhodes-Kropf <85074410+Benjamin-Rhodes-Kropf@users.noreply.github.com>
2022-09-03 01:43:53 -0400

    0.1.6
    -added start of color system

--- GeoSnap/Assets/Main/Scripts/ColorManager.cs
+++ GeoSnap/Assets/Main/Scripts/ColorManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class ColorManager : MonoBehaviour
 {
+    private CustomDebugManager _customDebugManager;
+    private Texture _myTexture;
+    
+    private void Awake()
+    {
+        _customDebugManager = CustomDebugManager.instance;
+        //_myTexture = gameObject.GetComponent<Texture>();
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
